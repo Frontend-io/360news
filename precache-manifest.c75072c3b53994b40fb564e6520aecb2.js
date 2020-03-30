20edb7a03546bcf5aea920c12c60551503c27dae
Iyobosa Jefferson Osagie <iyobosajefferson2@gmail.com>
2020-03-30 05:15:56 +0100

    Updates

---
+++ precache-manifest.c75072c3b53994b40fb564e6520aecb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22b60cb163b826b366eb0a50abb6c51",
+    "url": "/360news/index.html"
+  },
+  {
+    "revision": "599ee2ed7edb5c8293a1",
+    "url": "/360news/static/css/main.f7852a83.chunk.css"
+  },
+  {
+    "revision": "39a2e8f335d712869cb0",
+    "url": "/360news/static/js/2.3ebe8e6b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/360news/static/js/2.3ebe8e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599ee2ed7edb5c8293a1",
+    "url": "/360news/static/js/main.7348dc02.chunk.js"
+  },
+  {
+    "revision": "6b3dae89abc2b135e642",
+    "url": "/360news/static/js/runtime-main.3d4c0fcf.js"
+  }
+]);
